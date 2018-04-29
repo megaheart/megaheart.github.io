4cf01df8eb912a600f09f5fc01aee80fe83b587a
megaheart <megaheart2501@gmail.com>
2018-04-29 22:08:09 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+var data = [
+    {
+        "question":"How are you?",
+        "answers":[
+            "12",
+            "13",
+            "14",
+            "15"
+        ],
+        "correct":0,
+    },
+    {
+        "question":"Do you love me?",
+        "answers":[
+            "No",
+            "No,thanks",
+            "I miss you so much!",
+            "Hey, Yesssssss"
+        ],
+        "correct":2,   
+    },
+];
